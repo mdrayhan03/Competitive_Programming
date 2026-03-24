6d3717655d4dfd67e86b0c4d5ac2e5b1c93eb00b
mdrayhan03 <phylosopherhossain2022@gmail.com>
2026-03-24 12:52:22 +0600

    codeforce solving added

---
+++ CodeForce/158A_next_round.py
@@ -0,0 +1,54 @@
+'''
+A. Next Round
+time limit per test3 seconds
+memory limit per test256 megabytes
+"Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, as long as the contestant earns a positive score..." — an excerpt from contest rules.
+
+A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
+
+Input
+The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
+
+The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
+
+Output
+Output the number of participants who advance to the next round.
+
+Examples
+InputCopy
+8 5
+10 9 8 7 7 7 5 5
+OutputCopy
+6
+InputCopy
+4 2
+0 0 0 0
+OutputCopy
+0
+Note
+In the first example the participant on the 5th place earned 7 points. As the participant on the 6th place also earned 7 points, there are 6 advancers.
+
+In the second example nobody got a positive score.
+'''
+
+# solution
+
+import sys
+
+# Fast I/O
+data = sys.stdin.read().split()
+
+n, k = int(data[0]), int(data[1])
+scores = list(map(int, data[2:]))
+
+target = scores[k - 1]
+
+cnt = 0
+
+for ele in scores :
+    if ele > 0 and ele >= target :
+        cnt += 1
+    else :
+        break
+
+print(cnt)
---
+++ CodeForce/271A_beautiful_yr.py
@@ -0,0 +1,38 @@
+'''
+A. Beautiful Year
+time limit per test2 seconds
+memory limit per test256 megabytes
+It seems like the year of 2013 came only yesterday. Do you know a curious fact? The year of 2013 is the first year after the old 1987 with only distinct digits.
+
+Now you are suggested to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+
+Input
+The single line contains integer y (1000 ≤ y ≤ 9000) — the year number.
+
+Output
+Print a single integer — the minimum year number that is strictly larger than y and all it's digits are distinct. It is guaranteed that the answer exists.
+
+Examples
+InputCopy
+1987
+OutputCopy
+2013
+InputCopy
+2013
+OutputCopy
+2014
+'''
+
+# solution
+
+n = int(input())
+
+while True :
+    n += 1
+    year = str(n)
+
+    if len(set(year)) == len(year) :
+        print(year)
+        break
+    else :
+        continue
